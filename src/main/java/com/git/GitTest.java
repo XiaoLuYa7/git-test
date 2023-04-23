88b220d97942ed9ce41265a46023a9313bca9987
lu <3232414647@qq.com>
2023-04-23 16:20:16 +0800

    使用IDEA提交Git

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @Author: Lu
+ * @Date: 2023/4/23 16:11
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
